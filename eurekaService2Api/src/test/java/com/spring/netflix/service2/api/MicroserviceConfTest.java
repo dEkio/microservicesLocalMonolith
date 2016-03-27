2166794f45404b7e71132eef176894ef480ed443
sinisa229 <sinisa229@yahoo.com>
2016-03-28 01:37:07 +0200

    Added MicroserviceConfTest.java

---
+++ eurekaService2Api/src/test/java/com/spring/netflix/service2/api/MicroserviceConfTest.java
@@ -0,0 +1,21 @@
+package com.spring.netflix.service2.api;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@Ignore
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = MicroserviceConf.class)
+public class MicroserviceConfTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
